84492ccc5bc476062f7dbaf4eeb442da7bd893d2
Sasha <s.marchuk95@gmail.com>
2024-05-02 22:49:51 +0200

    drizzle stuff

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "./src/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: env.POSTGRES_URL,
+  },
+  tablesFilter: ["t3gallery_*"],
+} satisfies Config; 
