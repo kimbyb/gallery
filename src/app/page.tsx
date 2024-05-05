ccae521a2d5ae34371f58b34d6af4e3e32948751
Sasha <s.marchuk95@gmail.com>
2024-05-05 19:53:19 +0200

    signed out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,37 @@
 import Image from "next/image";
 import { db } from "../server/db";
 import { images } from "@/server/db/schema";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (<div className="flex flex-wrap gap-4">
+    {[...images, ...images, ...images].map((image, index) => (
+      <div key={image.id + "-" + index} className="flex w-48 flex-col">
+        <img src={image.url} />
+        <div>{image.name}</div>
+      </div>
+    ))}
+  </div>)
+}
+export default async function HomePage() {
+
 
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={image.id + "-" + index} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+
+      <SignedOut>
+        <div className="w-full h-full text-2xl text-center"> Please Sign In Above</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
